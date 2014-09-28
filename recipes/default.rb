a0836910ba3852042013978b91b1a1501d90e72e
Ross Perkins <ross@vubeology.com>
2014-09-28 09:43:35 -0700

    Now creates group when not exists

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,10 +19,13 @@ bash "download redis package" do
     cd "`dirname #{node['redis']['build_dir']}`" || exit 1
     mkdir "`basename #{node['redis']['build_dir']}`" || exit 2
   fi
+
   cd "#{node['redis']['build_dir']}" || exit 6
+
   # If the package_url is a Google Cloud Storage URL, then use gsutil to copy it
   if echo "#{node['redis']['package_url']}" | grep "^gs://" > /dev/null; then
     gsutil cp "#{node['redis']['package_url']}" redis.deb || exit 8
+
   # Else anything else is a URL that wget can fetch
   else
     wget "#{node['redis']['package_url']}" --output-document=redis.deb || exit 9
@@ -34,23 +37,23 @@ end
 bash "install redis" do
   cwd "#{node['redis']['build_dir']}"
   code <<-EOH
-  sudo dpkg -i redis.deb > redis.install_log 2>&1
+  sudo dpkg -i redis.deb > redis.install_log 2>&1 || exit 1
   EOH
   not_if { ::File.exist?(EXE_FILE) }
 end
 
 bash "create redis group" do
   code <<-EOH
-  sudo addgroup --system "#{node['redis']['user_group']}"
+  sudo addgroup --system "#{node['redis']['user_group']}" || exit 1
   EOH
   # But don't do this if the user group already exists
-  not_if "awk -F: '$1 == \"#{node['redis']['user_group']}\" {print $3}' /etc/group"
+  only_if "awk -F: '$1 == \"#{node['redis']['user_group']}\" {print $3}' /etc/group"
 end
 
 # Create a redis user if necessary
 bash "create redis user" do
   code <<-EOH
-  sudo adduser --system --no-create-home --ingroup "#{node['redis']['user_group']}" "#{node['redis']['username']}"
+  sudo adduser --system --no-create-home --ingroup "#{node['redis']['user_group']}" "#{node['redis']['username']}" || exit 1
   EOH
   # But don't do this if the username already exists
   not_if "id '#{node['redis']['username']}'"
